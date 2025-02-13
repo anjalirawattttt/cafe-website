157aa1f0d9e09dfa3e6ba0a27a18cd24fb51c7c2
Anjali Rawat <tweeanjalirawat@gmail.com>
2025-02-14 00:34:41 +0530

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,23 +43,7 @@ window.addEventListener("scroll", function () {
 
 
 
-/**
- * search box toggle
- */
 
-const searchBtn = document.querySelector("[data-search-btn]");
-const searchContainer = document.querySelector("[data-search-container]");
-const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
-const searchCloseBtn = document.querySelector("[data-search-close-btn]");
-
-const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
-
-for (let i = 0; i < searchBoxElems.length; i++) {
-  searchBoxElems[i].addEventListener("click", function () {
-    searchContainer.classList.toggle("active");
-    document.body.classList.toggle("active");
-  });
-}
 
 
 
@@ -67,26 +51,26 @@ for (let i = 0; i < searchBoxElems.length; i++) {
  * move cycle on scroll
  */
 
-const deliveryBoy = document.querySelector("[data-delivery-boy]");
+// const deliveryBoy = document.querySelector("[data-delivery-boy]");
 
-let deliveryBoyMove = -80;
-let lastScrollPos = 0;
+// let deliveryBoyMove = -80;
+// let lastScrollPos = 0;
 
-window.addEventListener("scroll", function () {
+// window.addEventListener("scroll", function () {
 
-  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+//   let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
 
-  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
-    let activeScrollPos = window.scrollY;
+//   if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+//     let activeScrollPos = window.scrollY;
 
-    if (lastScrollPos < activeScrollPos) {
-      deliveryBoyMove += 1;
-    } else {
-      deliveryBoyMove -= 1;
-    }
+//     if (lastScrollPos < activeScrollPos) {
+//       deliveryBoyMove += 1;
+//     } else {
+//       deliveryBoyMove -= 1;
+//     }
 
-    lastScrollPos = activeScrollPos;
-    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
-  }
+//     lastScrollPos = activeScrollPos;
+//     deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+//   }
 
-});
+// });
